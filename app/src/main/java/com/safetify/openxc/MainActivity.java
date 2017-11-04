41b0262246d46f16997320038b698136bf990248
Oğuzhan Gençel <gencelo@itu.edu.tr>
2017-11-04 14:41:51 +0300

    başlangıç

--- app/src/main/java/com/safetify/openxc/MainActivity.java
+++ app/src/main/java/com/safetify/openxc/MainActivity.java
@@ -1,13 +1,117 @@
 package com.safetify.openxc;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.openxc.VehicleManager;
+import com.openxc.measurements.EngineSpeed;
+import com.openxc.measurements.Measurement;
+import com.openxc.measurements.VehicleSpeed;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "StarterActivity";
+
+    private VehicleManager mVehicleManager;
+    private TextView mEngineSpeedView, mVehicleSpeedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // grab a reference to the engine speed text object in the UI, so we can
+        // manipulate its value later from Java code
+        mEngineSpeedView = (TextView) findViewById(R.id.engine);
+        mVehicleSpeedView = (TextView) findViewById(R.id.vehicle);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // When the activity goes into the background or exits, we want to make
+        // sure to unbind from the service to avoid leaking memory
+        if(mVehicleManager != null) {
+            Log.i(TAG, "Unbinding from Vehicle Manager");
+            // Remember to remove your listeners, in typical Android
+            // fashion.
+            mVehicleManager.removeListener(EngineSpeed.class,
+                    mSpeedListener);
+            unbindService(mConnection);
+            mVehicleManager = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // When the activity starts up or returns from the background,
+        // re-connect to the VehicleManager so we can receive updates.
+        if(mVehicleManager == null) {
+            Intent intent = new Intent(this, VehicleManager.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    VehicleSpeed.Listener mVehicleSteepListener = new VehicleSpeed.Listener() {
+
+        @Override
+        public void receive(Measurement measurement) {
+            final VehicleSpeed vehicleSpeed = (VehicleSpeed) measurement;
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mVehicleSpeedView.setText("Vehicle Speed (Kmp) :"
+                            + vehicleSpeed.getValue().doubleValue());
+                }
+            });
+        }
+    };
+    EngineSpeed.Listener mSpeedListener = new EngineSpeed.Listener() {
+        @Override
+        public void receive(Measurement measurement) {
+            final EngineSpeed speed = (EngineSpeed) measurement;
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    mEngineSpeedView.setText("Engine speed (RPM): "
+                            + speed.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        // Called when the connection with the VehicleManager service is
+        // established, i.e. bound.
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            Log.i(TAG, "Bound to VehicleManager");
+            // When the VehicleManager starts up, we store a reference to it
+            // here in "mVehicleManager" so we can call functions on it
+            // elsewhere in our code.
+            mVehicleManager = ((VehicleManager.VehicleBinder) service)
+                    .getService();
+
+            // We want to receive updates whenever the EngineSpeed changes. We
+            // have an EngineSpeed.Listener (see above, mSpeedListener) and here
+            // we request that the VehicleManager call its receive() method
+            // whenever the EngineSpeed changes
+            mVehicleManager.addListener(EngineSpeed.class, mSpeedListener);
+            mVehicleManager.addListener(VehicleSpeed.class, mVehicleSteepListener);
+        }
+
+        // Called when the connection with the service disconnects unexpectedly
+        public void onServiceDisconnected(ComponentName className) {
+            Log.w(TAG, "VehicleManager Service  disconnected unexpectedly");
+            mVehicleManager = null;
+        }
+    };
 }
